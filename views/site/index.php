a59f382eadcf3b04a40c58b4bd95e1130bc43d40
Gajen Sunthara <gajen.sunthara@gmail.com>
2015-01-29 11:19:24 -0500

    Basic API Controller and Classes

--- views/site/index.php
+++ views/site/index.php
@@ -1,50 +1,43 @@
 <?php
-/* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+	use yii\helpers\Url;
+	/* @var $this yii\web\View */
+	$this->title = 'CHPL OpenAPI';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>CHPL OpenAPI!</h1>
-        <p class="lead"></p>
-        <p><a class="btn btn-lg btn-success" href="chpl/">CHPL</a></p>
+        <h1>CHPL OpenAPI</h1>
+        <p class="lead">Certified Health Product List (CHPL)</p>
+        <p><a class="btn btn-lg btn-info" href="<? echo Url::to(['chpl/']); ?>">Search / Filter</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
 				<div class="panel panel-primary">
 					<div class="panel-heading">
-						<h3 class="panel-title">Panel primary</h3>
+						<h3 class="panel-title">OpenAPI - JSON</h3>
 					</div>
 					<div class="panel-body">
-						Panel content
+						<a class="btn btn-lg btn-info" target="_blank" href="http://localhost:8888/chpl/web/api?_format=json">JSON</a>
+						<p><pre>http://localhost:8888/chpl/web/api?_format=json</pre></p>
 					</div>
 				</div>
             </div>
 			
-			<div class="col-lg-4">
+			<div class="col-lg-6">
 				<div class="panel panel-success">
 					<div class="panel-heading">
-						<h3 class="panel-title">Panel success</h3>
+						<h3 class="panel-title">OpenAPI - XML</h3>
 					</div>
 					<div class="panel-body">
-						Panel content
+						<a class="btn btn-lg btn-success" target="_blank" href="http://localhost:8888/chpl/web/api?_format=xml">XML</a>
+						<p><pre>http://localhost:8888/chpl/web/api?_format=json</pre></p>
 					</div>
 				</div>
             </div>
             
-            <div class="col-lg-4">
-				<div class="panel panel-warning">
-					<div class="panel-heading">
-						<h3 class="panel-title">Panel warning</h3>
-					</div>
-					<div class="panel-body">
-						Panel content
-					</div>
-				</div>
-	        </div>
         </div> <!-- .row -->
 
     </div>
